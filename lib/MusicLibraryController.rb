f3e7289ab77c7704263f6716d472147842100dc9
Brent Bauman <brentbauman86@gmail.com>
2018-06-08 14:51:43 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -23,6 +23,16 @@ class MusicLibraryController
       case user
       when "list songs"
         list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "list song"
+        play_song
       end
     end
   end
